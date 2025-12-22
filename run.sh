d8acf77f5e436312f85bae25dd3d79c68ae580c0
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-12-22 17:13:47 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -107,7 +107,7 @@ fi
 
 check_configs
 
-for file in datas.txt wallets.txt proxies.txt; do
+for file in datas.txt proxies.txt; do
     if [ ! -f "$file" ]; then
         touch "$file"
         print_green "Created $file"
